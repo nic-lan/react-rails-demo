b78a56eb09c34bbfa65934d81f539a715ce9a7f3
nic-lan <nicolas.languille@gmail.com>
2017-01-06 18:25:45 +0100

    add clause guard to ApplicationControlelr#default_render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def default_render(*args)
+    raise ComponentNameMissingError unless @component_name.present?
+    raise DataMissingError unless @data.present?
+
     render "/react"
   end
+
+  private
+
+  class ComponentNameMissingError < StandardError; end
+  class DataMissingError < StandardError; end
 end
